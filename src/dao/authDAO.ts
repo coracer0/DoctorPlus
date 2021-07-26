ea212e7bc0c598b08131a596dcc1a2d80af04ea2
coracer0 <77650683+coracer0@users.noreply.github.com>
2021-07-26 12:43:45 -0500

    cambios en  consulta

--- src/dao/authDAO.ts
+++ src/dao/authDAO.ts
@@ -3,7 +3,7 @@ class AuthDAO {
   public async getUser(usuario: String) {
     
     const result = await pool.then(async (connection) => {
-      return await connection.query("SELECT idUsuario,usuario,password,idRol FROM usuario WHERE usuario = ?", [
+      return await connection.query("select u.idUsuario, u.usuario, u.password, u.idRol, r.descripcion as rol from usuario u INNER JOIN rol r on u.idRol = r.idRol FROM usuario WHERE usuario = ?", [
         usuario,
       ]);
     });
