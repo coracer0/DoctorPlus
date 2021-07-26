2e04ffc43311c0f108e71bb28ae9544e1b55dbfd
coracer0 <77650683+coracer0@users.noreply.github.com>
2021-07-26 13:24:55 -0500

    Correcion en las respuestas
    Se ha corregido la respuesta del back end

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -16,7 +16,7 @@ class AuthController{
 
         const usuarios = await dao.getUser(usuario);
 
-      
+        
 
         if (usuarios.length <=0){
             return res.status(400).json({message:'El usuario no existe'});
@@ -24,8 +24,8 @@ class AuthController{
 
         for (const usuario of usuarios) {
             if(await utils.checkPassword(password, usuario.password)){
-                const token = jwt.sign({idUsuario: usuario.idUsuario, usuario,idRol: usuario.idRol, rol: usuario.idRol},secretKey.jwtSecret,{expiresIn: '1h'});
-                return res.json({message:'OK',token,idUser: usuario.idUser,usuario,idRol: usuario.idRol, rol: usuario.idRol});
+                const token = jwt.sign({idUsuario: usuario.idUsuario, usuario,idRol: usuario.idRol, rol: usuario.rol},secretKey.jwtSecret,{expiresIn: '1h'});
+                return res.json({message:'OK',token,idUser: usuario.idUser,usuario,idRol: usuario.idRol, rol: usuario.rol});
             }else{
                 return res.status(400).json({message:"La contraseña es incorrecta"});
             }
